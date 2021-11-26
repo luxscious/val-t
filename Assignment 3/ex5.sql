cfe79652b5dc8fc406c20d4ba3756a0ba5652a97
luxscious <ggerges019@gmail.com>
2021-11-26 16:45:16 -0500

    clean up code

--- Assignment 3/ex5.sql
+++ Assignment 3/ex5.sql
@@ -3,56 +3,43 @@ SELECT COUNT(startDate)as Started
 FROM Tournament 
 WHERE DATE(startDate) < DATE('2021-11-25');
 
-
 -- Query 2
 SELECT username,pWin
 FROM Player
 ORDER BY pWin DESC;
 
 -- Query 3
-SELECT userId 
+CREATE VIEW usersFromWinningTeam AS (SELECT userId 
 				FROM TeamPlayers
-				WHERE teamId =129;
+				WHERE teamId = (SELECT teamId 
+								FROM Team 
+								WHERE
+								tName=(SELECT mWinner
+									   FROM VMatch
+									   WHERE tournamentId = 1 AND bracketLevel = (SELECT maxBracketLevel 
+                                                                                  FROM Tournament 
+                                                                                  WHERE TournamentId = '1'))));
+SELECT username FROM Player RIGHT JOIN usersFromWinningTeam ON Player.userId = usersFromWinningTeam.userId;
 -- Query 4
--- CREATE VIEW usersFromWinningTeam AS (SELECT userId 
--- 				FROM TeamPlayers
--- 				WHERE teamId = (SELECT teamId 
--- 								FROM Team 
--- 								WHERE
--- 								tName=(SELECT mWinner
--- 											 FROM VMatch
--- 											 WHERE tournamentId = 1 AND bracketLevel = (SELECT maxBracketLevel FROM Tournament where TournamentId = '1'))));
--- SELECT username FROM Player RIGHT JOIN usersFromWinningTeam ON Player.userId = usersFromWinningTeam.userId;
--- Query 4
--- SELECT aName,aType,aWins
--- FROM Agent
--- WHERE aWins > (SELECT AVG(aWins) as avg
--- 			   FROM Agent);
+SELECT aName,aType,aWins
+FROM Agent
+WHERE aWins > (SELECT AVG(aWins) as avg
+			   FROM Agent);
 -- Query 5
--- SELECT MAX(tWins), tName
--- FROM team
--- (SELECT tWins
--- 				  FROM Team
--- 				  WHERE teamId = 
-								-- (SELECT teamId 
--- 								  FROM Roster
---                                   WHERE matchId = 
-											-- (SELECT matchId 
--- 												   FROM VMatch
---                                                    WHERE tournamentId= 
-	
- --    CREATE VIEW tournamentsNA AS (SELECT tournamentId
--- 								  FROM Tournament
--- 								  WHERE region ='North America');
+CREATE VIEW tournamentsNA AS (SELECT tournamentId
+								  FROM Tournament
+								  WHERE region ='North America');
+CREATE VIEW matchesInNA AS (
+			SELECT matchId FROM VMatch RIGHT JOIN tournamentsNA ON  VMatch.tournamentId =tournamentsNa.tournamentId);
 
--- CREATE VIEW matchesInNA AS (SELECT matchId FROM VMatch RIGHT JOIN tournamentsNA ON  VMatch.tournamentId =tournamentsNa.tournamentId);
--- DROP VIEW teamsInNA;
- -- CREATE VIEW teamsInNA AS (SELECT DISTINCT(teamId) FROM Roster RIGHT JOIN matchesInNA ON Roster.matchId = matchesInNA.matchId);
--- CREATE VIEW teamInfo AS (SELECT Team.tName, Team.tWins FROM Team  RIGHT JOIN teamsInNA ON Team.teamId = teamsInNA.teamId);
+CREATE VIEW teamsInNA AS (
+			SELECT DISTINCT(teamId) FROM Roster RIGHT JOIN matchesInNA ON Roster.matchId = matchesInNA.matchId);
+
+CREATE VIEW teamInfo AS (
+            SELECT Team.tName, Team.tWins FROM Team  RIGHT JOIN teamsInNA ON Team.teamId = teamsInNA.teamId);
 SELECT tName, tWins FROM teamInfo WHERE tWins = (SELECT Max(tWins) FROM teamInfo);                                                              
 -- Query 6
 SELECT tName FROM Team WHERE teamId NOT IN (SELECT teamId from Roster);
-
 -- Query 7
 SELECT tName,tLosses
 FROM Team WHERE tLosses = 0 ;
--- Assignment 3/import tables.sql
+++ Assignment 3/import tables.sql
@@ -1,36 +1,36 @@
--- LOAD DATA INFIlE "users.csv"
--- INTO TABLE User
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+LOAD DATA INFIlE "users.csv"
+INTO TABLE User
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
--- LOAD DATA INFIlE "players.csv"
--- INTO TABLE Player
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+LOAD DATA INFIlE "players.csv"
+INTO TABLE Player
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
--- LOAD DATA INFIlE "tournaments.csv"
--- INTO TABLE Tournament
--- FIELDS TERMINATED BY ','
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+LOAD DATA INFIlE "tournaments.csv"
+INTO TABLE Tournament
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
--- LOAD DATA INFIlE "teams.csv"
--- INTO TABLE Team
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY ''
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+LOAD DATA INFIlE "teams.csv"
+INTO TABLE Team
+FIELDS TERMINATED BY ','
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
--- LOAD DATA INFIlE "matches.csv"
--- INTO TABLE VMatch
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY ''
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+LOAD DATA INFIlE "matches.csv"
+INTO TABLE VMatch
+FIELDS TERMINATED BY ','
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 LOAD DATA INFIlE "rosters.csv"
 INTO TABLE Roster
@@ -46,40 +46,27 @@ ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-INSERT INTO Agent VALUES ("Brimstone","Controller",0,0);
+INSERT INTO Agent VALUES ("Brimstone","Controller",10,0);
 INSERT INTO Agent VALUES ("Viper","Controller",0,0);
 INSERT INTO Agent VALUES ("Omen","Controller",0,0);
 INSERT INTO Agent VALUES ("Astra","Controller",0,0);
 INSERT INTO Agent VALUES ("Killjoy","Sentinel",0,0);
 INSERT INTO Agent VALUES ("Cypher","Sentinel",0,0);
-INSERT INTO Agent VALUES ("Sage","Sentinel",0,0);
+INSERT INTO Agent VALUES ("Sage","Sentinel",3,0);
 INSERT INTO Agent VALUES ("Chamber","Sentinel",0,0);
-INSERT INTO Agent VALUES ("Sova","Initiator",0,0);
+INSERT INTO Agent VALUES ("Sova","Initiator",2,0);
 INSERT INTO Agent VALUES ("KAY/O","Initiator",0,0);
 INSERT INTO Agent VALUES ("Breach","Initiator",0,0);
 INSERT INTO Agent VALUES ("Skye","Initiator",0,0);
-INSERT INTO Agent VALUES ("Jett","Duelist",0,0);
+INSERT INTO Agent VALUES ("Jett","Duelist",10,0);
 INSERT INTO Agent VALUES ("Raze","Duelist",0,0);
 INSERT INTO Agent VALUES ("Pheonix","Duelist",0,0);
 INSERT INTO Agent VALUES ("Reyna","Duelist",0,0);
-INSERT INTO Agent VALUES ("Yoru","Duelist",0,0);
+INSERT INTO Agent VALUES ("Yoru","Duelist",7,0);
 
-UPDATE Agent
-SET aWins = 10
-WHERE aName = 'Jett';
-UPDATE Agent
-SET aWins = 7
-WHERE aName = 'Astra';
-UPDATE Agent
-SET aWins = 20
-WHERE aName = 'Yoru';
-UPDATE Agent
-SET aWins = 2
-WHERE aName = 'Sage';
-
-SELECT AVG(aWins) FROM Agent;
-
-
--- INSERT INTO teamsponsored VALUES(0,10,200);
-
--- INSERT INTO teamsponsored VALUES(0,10,200);
+INSERT INTO User VALUES (1001,'company1','fakeemail@gmail.com','1234564');
+INSERT INTO User VALUES (1002,'company2','fakeemail2@gmail.com','12343564');
+INSERT INTO Sponsor VALUES(1001,0);
+INSERT INTO Sponsor VALUES(1002,0);
+INSERT INTO SponsoredPlayer VALUES(1,1001);
+INSERT INTO SponsoredPlayer VALUES(2,1002);
