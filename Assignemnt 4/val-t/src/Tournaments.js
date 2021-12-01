0245d0b21c830d05830ae8d13c9a23cc8f48f49f
luxscious <ggerges019@gmail.com>
2021-12-01 18:57:55 -0500

    Pages Created

---
+++ Assignemnt 4/val-t/src/Tournaments.js
@@ -0,0 +1,3 @@
+export default function Tournaments() {
+  return <div>Tournaments</div>;
+}
--- Assignemnt 4/val-t/src/index.js
+++ Assignemnt 4/val-t/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Leaderboard from "./Leaderboard";
+import Tournaments from "./Tournaments";
+import Profile from "./Profile";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/leaderboard" element={<Leaderboard />} />
+      <Route path="/tournaments" element={<Tournaments />} />
+      <Route path="/profile/:username" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
