e6c3685b6e637733bf77ea7e94d5666dfbfb6d67
kliao25 <kliao25@uwo.ca>
2021-12-05 00:17:55 -0500

    Sign-up init

--- Assignemnt 4/val-t/src/pages/SignUp.js
+++ Assignemnt 4/val-t/src/pages/SignUp.js
@@ -1,6 +1,10 @@
 import bg from "../assets/bg_home.svg";
-import logo from "../assets/val-t-text.svg";
 import { makeStyles, Button } from "@material-ui/core";
+import whiteRect from "../assets/signup-rect.svg"
+import logo from "../assets/val-t-text.svg";
+import { color } from "@mui/system";
+import { white } from "material-ui/styles/colors";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   //put css here
@@ -25,12 +29,33 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     marginBottom: 65, //Toolbar takes up 65px
   },
+
+  loginRectangle: {
+    position: "fixed",
+    padding: 0,
+    margin: 0,
+
+    borderRadius: 30,
+
+    width: "60%",
+    height: "60%",
+    backgroundImage: `url(${whiteRect})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover ",
+    zIndex: "-1",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
   button: {
     alignSelf: "center",
     textDecoration: "none",
     width: 209,
     height: 45,
   },
+
   buttonText: {
     marginTop: 40,
     padding: 8,
@@ -46,7 +71,25 @@ const useStyles = makeStyles((theme) => ({
       border: 1,
       borderColor: "#FF4655",
     },
-  },
+  }, 
+input: {
+
+  color: "white",
+  width: "30%", 
+  height: "5.5%",
+  borderRadius: 6,
+  fontFamily: "Mark Pro",
+  backgroundPosition: "center",
+  display: "flex",
+  flexDirection: "column",
+
+},
+inputText: {
+  backgroundColor: '#FFFFFF',
+  textAlign: 'center',
+  fontSize: '16px',
+  fontFamily: "Mark Pro",
+}, 
 }));
 
 
