b2f5897cd1dda3d89c8c8b6d16ab80d4bb9c2691
Areej Ansari <areej.ansari@uwo.ca>
2021-12-05 21:58:49 -0500

    Added Create Account and Drop Down
    Need to still add login

--- Assignemnt 4/val-t/src/pages/SignUp.js
+++ Assignemnt 4/val-t/src/pages/SignUp.js
@@ -6,6 +6,24 @@ import { TextField } from "@mui/material";
 import { color, flexbox } from "@mui/system";
 import { white } from "material-ui/styles/colors";
 import { Link } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+
+const userType = [
+  {
+    value: 'PLAYER',
+    label: 'PLAYER',
+  },
+  {
+    value: 'SUBSCRIBER',
+    label: 'SUBSCRIBER',
+  },
+  {
+    value: 'SPONSOR',
+    label: 'SPONSOR',
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   //put css here
@@ -35,7 +53,7 @@ const useStyles = makeStyles((theme) => ({
   },
 
   nameContainer: {
-    paddingTop: 88,
+    paddingTop: 0,
     paddingLeft: 150,
     paddingRight: 150,
     paddingBottom: 18,
@@ -121,15 +139,34 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 6,
     fontFamily: "Mark Pro",
     width: "100%",
+    backgroundColor: "white",
   },
+
+  h1:{
+    alignItems: "center",
+    borderRadius: 6,
+    justifyContent: "space-between",
+    fontFamily: "Mark Pro",
+    fontSize:40,
+    color:"#FF4655",
+    textAlign:'center'
+  }
+
 }));
 
 export default function SignUp() {
+  const [userTypes, setUserType] = React.useState('');
+
+  const handleChange = (event) => {
+    setUserType(event.target.value);
+  };
   const classes = useStyles();
   return (
     <div className={classes.page}>
+   
       <div className={classes.container}>
-        <div className={classes.nameContainer}>
+      <h1 className ={classes.h1} >CREATE ACCOUNT</h1> 
+        <div className={classes.nameContainer}> 
           <TextField
             id="first-name"
             className={classes.inputFirstName}
@@ -159,6 +196,7 @@ export default function SignUp() {
           <TextField
             id="email"
             className={classes.inputEmail}
+        
             style={{ backgroundColor: "white", marginBottom: 20 }}
             InputLabelProps={{
               style: { fontFamily: "Mark Pro", fontSize: "18px" },
@@ -179,18 +217,26 @@ export default function SignUp() {
             variant="filled"
             InputProps={{ disableUnderline: true }}
           />
-
-          <TextField
-            id="userType"
-            className={classes.userDropdown}
-            style={{ backgroundColor: "white" }}
-            InputLabelProps={{
-              style: { fontFamily: "Mark Pro", fontSize: "18px" },
+            
+<div className={classes.userDropdown}>
+        <TextField className={classes.userDropdown} InputLabelProps={{
+              style: { fontFamily: "Mark Pro", fontSize: "18px", width:"100%" },
             }}
-            label="SELECT USER TYPE"
-            variant="filled"
-            InputProps={{ disableUnderline: true }}
-          />
+          select label=""
+          label ="SELECT USER TYPE"
+          variant="filled"
+          root=""
+          value={userTypes}
+          onChange={handleChange}
+        >
+          {userType.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
+          
 
           <div className={classes.button}>
             <Link to="/profile" className={classes.button}>
